7c8e993f12b04d1227f995641dc638aa6713f1d8
Mikhail Romanov <mikhail.romanov@aalto.fi>
2024-11-10 00:42:00 +0200

    added password and limit checker

--- bot.py
+++ bot.py
@@ -1,23 +1,35 @@
 import os
 from telegram import Update
-from telegram.ext import Application, CommandHandler, CallbackContext, ContextTypes
+from telegram.ext import Application, CommandHandler, ConversationHandler, ContextTypes, MessageHandler, filters
 from inventory import VakoInvis  # Make sure this imports the class you've defined above
 
 # Initialize the inventory manager
-inventory_manager = VakoInvis("inventory.txt")
+inventory_manager = VakoInvis("inventory.json")
 
 TOKEN = open('token.txt', "r").readline().strip()
+PASSWORD = open('password.txt', "r").readline().strip()
 
+PASSWORD_CHECK = 0
 
 # Define command handlers
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Welcome to the Vako inventory Bot! Use /help to see available commands.")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    await update.message.reply_text("Welcome to the Vako inventory Bot! Please enter the password to continue:")
+    return PASSWORD_CHECK
+
+async def check_password(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    if update.message.text == PASSWORD:
+        await update.message.reply_text("Password correct! Use /help to see available commands.")
+        return ConversationHandler.END
+    else:
+        await update.message.reply_text("Incorrect password. Please try again:")
+        return PASSWORD_CHECK
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
         "/add <item> <quantity> [new]- Add quantity to an item. Add 'new' at the end of the message if you want to add fully new item\n"
         "/remove <item> <quantity> [totally]- Remove quantity from an item. Add totally if you want totally remove the item from the database\n"
-        "/view [item] - View inventory or a specific item.\n"
+        "/view [item] [full] - View inventory or a specific item.\n"
+        "/limit <item> <new limit> - change alarm limit of the item"
         "/help - Show this help message"
     )
 
@@ -41,31 +53,55 @@ async def remove_item(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
         quantity = int(context.args[1])
         totally = len(context.args) > 2 and context.args[2].lower() == "totally"
         
-        if inventory_manager.remove(item, quantity):
-            await update.message.reply_text(f"Removed {quantity} of '{item}' from the inventory.")
+        if inventory_manager.remove(item, quantity, totally=totally):
+            alarm = inventory_manager.check_alarm_limit(item)
+            await update.message.reply_text(f"Removed {quantity} of '{item}' from the inventory."
+                                            f"{'\nALARM! QUANTITY OF THIS ITEM IS LESS THEN ALARM LIMIT! CONTACT CHAIR ABOUT THIS PROBLEM' if alarm == False else ''}")
         else:
             await update.message.reply_text(f"Error: Item '{item}' not found in inventory.")
     
     except (IndexError, ValueError):
-        await update.message.reply_text("Usage: /remove <item> <quantity>")
+        await update.message.reply_text("Usage: /remove <item> <quantity> [totally]")
 
 async def view_inventory(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if context.args:
         item = context.args[0]
+        full = len(context.args) > 1 and context.args[1].lower() == "full"
         if item in inventory_manager.inventory:
-            await update.message.reply_text(f"{item} => {inventory_manager.inventory[item]}")
+            await update.message.reply_text(f"{item}:\n   quantity: {inventory_manager.inventory[item]['quantity']}\n   "
+                                            f"{f'alarm limit : {inventory_manager.inventory[item]['alarm_limit']}' if full else ''}")
         else:
             await update.message.reply_text(f"Item '{item}' not found in inventory.")
     else:
-        inventory_list = "\n".join([f"{item} => {quantity}" for item, quantity in inventory_manager.inventory.items()])
-        await update.message.reply_text("Inventory:\n" + (inventory_list if inventory_list else "Inventory is empty."))
+        inventory_list = "\n".join([f"{item} =>\n   quantity: {details['quantity']}\n" for item, details in inventory_manager.inventory.items()])
+        await update.message.reply_text("\n" + (inventory_list if inventory_list else "Inventory is empty."))
+
+async def limit_item(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    try:
+        item = context.args[0]
+        limit = context.args[1]
+        if inventory_manager.update_alarm_limit(item, limit):
+            await update.message.reply_text(f"Change limit of the {item} to {limit}")
+        else:
+            await update.message.reply_text(f"Error: Item '{item}' not found")
+    except (IndexError, ValueError):
+        await update.message.reply_text(f"Usage: /limit <item> <new limit>")
 
 # Main function to start the bot
 def main():
     # Replace 'YOUR_TOKEN' with your actual bot token
     application = Application.builder().token(TOKEN).build()
 
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            PASSWORD_CHECK: [MessageHandler(filters.TEXT & ~filters.COMMAND, check_password)],
+        },
+        fallbacks=[],
+    )
+
     # Register command handlers
+    application.add_handler(conv_handler)
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("add", add_item))
